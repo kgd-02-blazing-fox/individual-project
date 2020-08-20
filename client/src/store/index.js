e194c283572fd8df87fc79cd7600da102ca80533
arissatya <arissatya.nugraha@gmail.com>
2020-08-21 00:10:46 +0700

    init vue

---
+++ client/src/store/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import ServerAPI from '../axios/ServerAPI'
+import router from '../router'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    items: []
+  },
+  mutations: {
+    SET_ITEMS (state, payload) {
+      state.items = payload
+    }
+  },
+  actions: {
+    fetchProducts (context) {
+      ServerAPI({
+        method: 'GET',
+        url: '/'
+      })
+        .then(({ data }) => {
+          context.commit('SET_ITEMS', data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally((_) => {
+          console.log('FETCHED')
+        })
+    },
+    updateProduct (context, data) {
+      ServerAPI({
+        method: 'PUT',
+        url: '/' + data.id,
+        data: {
+          name: data.name,
+          image_url: data.image_url,
+          price: data.price,
+          stock: data.stock
+        },
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        }
+      })
+        .then(({ data }) => {
+          context.dispatch('fetchProducts')
+          router.push('/')
+        })
+        .catch((err) => {
+          console.log(err)
+          router.push(`/`)
+        })
+        .finally((_) => {
+          console.log('what?')
+        })
+    }
+  }
+})
